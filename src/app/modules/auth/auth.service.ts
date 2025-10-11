ecfae7ffac537083afb1133f82a110825e3bd0a1
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-10-11 22:29:44 +0600

    57-9 Generating JWT Token

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { UserStatus } from "@prisma/client"
 import { prisma } from "../../shared/prisma"
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken'
 
 const login = async (payload: { email: string, password: string }) => {
+    console.log(payload)
     const user = await prisma.user.findUniqueOrThrow({
         where: {
             email: payload.email,
@@ -10,12 +12,30 @@ const login = async (payload: { email: string, password: string }) => {
         }
     })
 
+    console.log(user)
+
     const isCorrectPassword = await bcrypt.compare(payload.password, user.password)
 
-    if(isCorrectPassword){
+    if(!isCorrectPassword){
         throw new Error("Password Incorrect")
     }
-    
+    //  generate access token 
+    const accessToken = jwt.sign({email: user.email, role: user.role}, "abc", {
+        algorithm:"HS256",
+        expiresIn :"1h"
+    })
+
+    // generate refresh token 
+        const refreshToken = jwt.sign({email: user.email, role: user.role}, "abc", {
+        algorithm:"HS256",
+        expiresIn :"90d"
+    })
+
+
+    return {
+        accessToken,
+        refreshToken
+    }
 }
 
 export const AuthServices = {
