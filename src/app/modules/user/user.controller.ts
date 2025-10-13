3ae561ccd9b61a5115ca70a619e18605958569ab
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-10-13 09:54:13 +0600

    58-2 Fetch All Users with Searching and Sorting

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -41,8 +41,8 @@ const createDoctor = catchAsync(async (req: Request, res: Response) => {
 });
 
 const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
-    const { page, limit } = req.query
-    const result = await UserService.getAllFromDB({ page: Number(page), limit: Number(limit) })
+    const { page, limit, searchTerm, sortBy, sortOrder } = req.query
+    const result = await UserService.getAllFromDB({ page: Number(page), limit: Number(limit), searchTerm, sortBy, sortOrder })
     sendResponse(res, {
         statusCode: 200,
         success: true,
