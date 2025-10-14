5b7cdd8494a64967a807ca13c5d8c45ea98a461e
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-10-14 20:18:39 +0600

    59-3 Creating Schedule – Part 1, 59-4 Creating Schedule – Part 2,3

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { UserRoutes } from '../modules/user/user.routes';
 import { AuthRoutes } from '../modules/auth/auth.route';
+import { ScheduleRoutes } from '../modules/schedule/schedule.routes';
 
 
 const router = express.Router();
@@ -13,6 +14,10 @@ const moduleRoutes = [
     {
         path: '/auth',
         route: AuthRoutes
+    },
+    {
+        path: '/schedule',
+        route: ScheduleRoutes
     }
 ];
 
