b13577e66bed4e79c453d9671de1427976399439
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-10-15 10:09:52 +0600

    59-10 Fixing & Enhancing Get Available Schedule for Doctor Functionality

--- src/app/modules/schedule/schedule.controller.ts
+++ src/app/modules/schedule/schedule.controller.ts
@@ -3,6 +3,7 @@ import catchAsync from "../../shared/catchAsync";
 import sendResponse from "../../shared/sendResponse";
 import { ScheduleService } from "./schedule.service";
 import pick from "../../helper/pick";
+import { IJWTPayload } from '../../types/common';
 
 
 
@@ -18,10 +19,12 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
         data: result
     })
 })
-const schedulesForDoctor = catchAsync(async (req: Request, res: Response) => {
+const schedulesForDoctor = catchAsync(async (req: Request & {user?:IJWTPayload}, res: Response) => {
     const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]) // pagination and sorting
     const filters = pick(req.query, ["startDateTime", "endDateTime"])
-    const result = await ScheduleService.schedulesForDoctor(filters, options)
+
+    const user = req.user
+    const result = await ScheduleService.schedulesForDoctor(user as IJWTPayload, filters, options)
 
     sendResponse(res, {
         statusCode: 201,
