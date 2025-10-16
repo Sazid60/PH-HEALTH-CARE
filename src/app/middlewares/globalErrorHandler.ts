5ebfe2a9217fb5987ce4080647ce772d3c02c33e
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-10-16 15:16:45 +0600

    60-2 Prisma Error Handling – Part 2

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -4,7 +4,7 @@ import httpStatus from "http-status"
 
 const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 
-    let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+    let statusCode : number = httpStatus.INTERNAL_SERVER_ERROR;
     let success = false;
     let message = err.message || "Something went wrong!";
     let error = err;
@@ -12,20 +12,38 @@ const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFun
     if(error instanceof Prisma.PrismaClientKnownRequestError){
         if(error.code == "P2002"){
             message = "Duplicate key error",
-            error = error.meta 
+            error = error.meta,
+            statusCode = httpStatus.CONFLICT
         }
         if(error.code == "P1000"){
             message = "Authentication failed against database server!",
-            error = error.meta 
+            error = error.meta,
+            statusCode = httpStatus.BAD_GATEWAY
         }
         if(error.code == "P2003"){
             message = "Foreign key constraint failed on the field!",
-            error = error.meta 
+            error = error.meta,
+            statusCode = httpStatus.BAD_REQUEST
         }
-        if(error.code == "P2007"){
-            message = "Data validation error!",
-            error = error.meta 
-        }
+
+    }
+
+    else if(error instanceof Prisma.PrismaClientValidationError){
+        message = "Validation Error"
+        error = error.message,
+        statusCode = httpStatus.BAD_REQUEST
+    }
+
+    
+    else if(error instanceof Prisma.PrismaClientUnknownRequestError){
+        message = "Something Unknown Error Happened!"
+        error = error.message
+        statusCode = httpStatus.BAD_REQUEST
+    }
+    else if(error instanceof Prisma.PrismaClientInitializationError){
+        message = "Prisma Client Failed To Initialize!"
+        error = error.message
+        statusCode = httpStatus.BAD_REQUEST
     }
 
     res.status(statusCode).json({
