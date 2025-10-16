e4a696adefcd8da00c591f75ded629bc84f1e49d
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-10-16 12:16:46 +0600

    60-1 Prisma Error Handling – Part 1

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express"
 import httpStatus from "http-status"
 
@@ -7,6 +8,25 @@ const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFun
     let success = false;
     let message = err.message || "Something went wrong!";
     let error = err;
+
+    if(error instanceof Prisma.PrismaClientKnownRequestError){
+        if(error.code == "P2002"){
+            message = "Duplicate key error",
+            error = error.meta 
+        }
+        if(error.code == "P1000"){
+            message = "Authentication failed against database server!",
+            error = error.meta 
+        }
+        if(error.code == "P2003"){
+            message = "Foreign key constraint failed on the field!",
+            error = error.meta 
+        }
+        if(error.code == "P2007"){
+            message = "Data validation error!",
+            error = error.meta 
+        }
+    }
 
     res.status(statusCode).json({
         success,
