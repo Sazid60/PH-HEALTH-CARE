41e4a2f9c0f2300e695b99c633dbd0068d77d2a0
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-10-17 23:11:12 +0600

    61-2 Implementing AI-Driven Doctor Suggestion – Part 1

--- src/app/modules/doctor/doctor.service.ts
+++ src/app/modules/doctor/doctor.service.ts
@@ -4,6 +4,9 @@ import { IOptions, paginationHelper } from "../../helper/paginationHelper";
 import { prisma } from "../../shared/prisma";
 import { doctorSearchableFields } from "./doctor.constant";
 import { IDoctorUpdateInput } from "./doctor.interface";
+import ApiError from "../../errors/ApiError";
+import httpStatus from 'http-status';
+import { openai } from "../../helper/open-router";
 
 
 const getAllFromDB = async (filters: any, options: IOptions) => {
@@ -136,11 +139,65 @@ const updateIntoDB = async (id: string, payload: Partial<IDoctorUpdateInput>) =>
     })
 
 
+};
+
+const getAISuggestions = async (payload: { symptoms: string }) => {
+    // implement ai suggestion system 
+
+    console.log(payload)
+
+    if (!(payload && payload.symptoms)) {
+        throw new ApiError(httpStatus.BAD_REQUEST, "Symptom is Required!")
+    }
+
+    const doctors = await prisma.doctor.findMany({
+        where: {
+            isDeleted: false
+        },
+        include: {
+            doctorSpecialties: {
+                include: {
+                    specialities: true
+                }
+            }
+        }
+    })
+
+    const prompt = `
+You are a medical assistant AI. Based on the patient's symptoms, suggest the top 3 most suitable doctors.
+Each doctor has specialties and years of experience.
+Only suggest doctors who are relevant to the given symptoms.
+
+Symptoms: ${payload.symptoms}
+
+Here is the doctor list (in JSON):
+${JSON.stringify(doctors, null, 2)}
+
+Return your response in JSON format with full individual doctor data. 
+`;
+
+const completion = await openai.chat.completions.create({
+    model: 'z-ai/glm-4.5-air:free',
+    messages: [
+      {
+        role: 'system',
+        content:  "You are a helpful AI medical assistant that provides doctor suggestions.",
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+  });
+
+
+    console.log(doctors)
 }
 
 export const DoctorService = {
     getAllFromDB,
-    updateIntoDB
+    updateIntoDB,
+    getAISuggestions
 }
 
 
